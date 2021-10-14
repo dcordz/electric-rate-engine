6cd0bcaad0f2378fce878f488fe48c26d3bcb5fb
Brian Knoles <bknoles@gmail.com>
2021-10-14 14:39:42 -0500

    Add a user-assignable billingCategory property to RateElements

--- src/rateEngine/RateElement.ts
+++ src/rateEngine/RateElement.ts
@@ -14,6 +14,7 @@ export interface RateElementInterface {
   rateElementType: RateElementType;
   rateComponents: Array<RateComponentInterface & BillingDeterminantFactoryInterface>;
   name: string;
+  billingCategory: BillingCategory;
 }
 
 export enum RateElementClassification {
@@ -22,16 +23,28 @@ export enum RateElementClassification {
   FIXED = 'fixed',
 };
 
+// Since the billing category is passed into a RateCalculator as part of the
+// arguments, this enum exists for documentation rather than actual type safety.
+// Once this compiles to JS, nothing will prevent an application from passing in
+// an abitrary string for the billingCategory.
+export enum BillingCategory {
+  TAX = 'tax',
+  SUPPLY = 'supply',
+  DELIVERY = 'delivery',
+}
+
 class RateElement {
   private _rateComponents: Array<RateComponent>;
   name: string;
   type: RateElementType;
   classification?: RateElementClassification;
+  billingCategory?: BillingCategory;
   errors: Array<Error> = [];
 
-  constructor({ rateElementType, name, rateComponents }: RateElementInterface, loadProfile: LoadProfile) {
+  constructor({ rateElementType, name, rateComponents, billingCategory }: RateElementInterface, loadProfile: LoadProfile) {
     this.name = name;
     this.type = rateElementType;
+    this.billingCategory = billingCategory;
 
     this._rateComponents = rateComponents.map(({ charge, name, ...rest }) => {
       const billingDeterminants = BillingDeterminantFactory.make(rateElementType, { ...rest }, loadProfile);
