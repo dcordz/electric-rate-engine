7e61524b2f5f884a457a32fc3c6bf9a6ee5a2edb
Jake Rogers <jake@bellawatt.com>
2021-10-29 16:01:32 -0400

    Improve LoadProfileScaler goal seek

--- src/rateEngine/LoadProfileScaler.ts
+++ src/rateEngine/LoadProfileScaler.ts
@@ -10,12 +10,12 @@ class LoadProfileScaler {
 
   constructor(loadProfile) {
     this.loadProfile = loadProfile;
-  };
+  }
 
   to(scaler: number): LoadProfile {
     return new LoadProfile(
-      this.loadProfile.expanded().map(loadHour => loadHour.load * scaler),
-      {year: this.loadProfile.year}
+      this.loadProfile.expanded().map((loadHour) => loadHour.load * scaler),
+      { year: this.loadProfile.year },
     );
   }
 
@@ -24,58 +24,61 @@ class LoadProfileScaler {
     return this.to(scaler);
   }
 
-  toAverageMonthlyBill(
-    amount: number,
-    rate: RateInterface
-  ): LoadProfile {
-    // Need this as a percent for better resolution within the algorithm
-    const scalerAsPercent = 100;
-    const fnParams = [scalerAsPercent, rate, this];
+  toAverageMonthlyBill(amount: number, rate: RateInterface): LoadProfile {
+    const magnitude = Math.max(Math.floor(Math.log10(Math.abs(amount))), 0);
+
+    const orderOfMagnitude = 10 ** magnitude;
+    const initialScalerGuess = orderOfMagnitude;
+    const fnParams = [initialScalerGuess, rate, this, magnitude];
 
     try {
-      const finalScalerAsPercent = goalSeek({
+      const finalScaler = goalSeek({
         fn: this.scaledMonthlyCost,
         fnParams,
         percentTolerance: 0.1,
         maxIterations: 1000,
-        maxStep: 50,
+        maxStep: 0.5 * orderOfMagnitude,
         goal: amount,
-        independentVariableIdx: 0
+        independentVariableIdx: 0,
       });
-      return this.to(finalScalerAsPercent / 100);
+
+      const scalerAsDecimal = finalScaler / orderOfMagnitude;
+      return this.to(scalerAsDecimal);
     } catch (e) {
-      throw(e);
+      throw e;
     }
   }
 
   toMonthlyKwh(monthlyKwh: Array<number>): LoadProfile {
     if (monthlyKwh.length !== 12) {
-      throw('monthlyKwh must be an array of 12 numbers');
+      throw 'monthlyKwh must be an array of 12 numbers';
     }
     const scalersByMonth = this.loadProfile.sumByMonth().map((kwh, idx) => {
       return monthlyKwh[idx] / kwh;
     });
-    const scaledLoad = this.loadProfile.expanded().map(loadHour => {
+    const scaledLoad = this.loadProfile.expanded().map((loadHour) => {
       return {
         ...loadHour,
         load: loadHour.load * scalersByMonth[loadHour.month],
       };
     });
-    return new LoadProfile(scaledLoad, {year: this.loadProfile.year});
+    return new LoadProfile(scaledLoad, { year: this.loadProfile.year });
   }
 
   private scaledMonthlyCost(
-    scalerAsPercent: number,
+    scaler: number,
     rate: RateInterface,
     context: LoadProfileScaler,
+    magnitude: number,
   ): number {
-    const scaledLoadProfile = context.to(scalerAsPercent / 100);
+    const scaledLoadProfile = context.to(scaler / 10 ** magnitude);
     const rateCalculator = new RateCalculator({
       ...rate,
       loadProfile: scaledLoadProfile,
     });
+
     return rateCalculator.annualCost() / 12;
-  };
-};
+  }
+}
 
 export default LoadProfileScaler;
--- src/rateEngine/__tests__/LoadProfileScaler.unmocked.test.ts
+++ src/rateEngine/__tests__/LoadProfileScaler.unmocked.test.ts
@@ -4,11 +4,49 @@ import sum from 'lodash/sum';
 import LoadProfile from '../LoadProfile';
 //import goalSeek from 'goal-seek';
 import e1 from '../__mocks__/rates/e-1';
+import RateCalculator, { RateCalculatorInterface } from '../RateCalculator';
 
 const getLoadProfileOfOnes = () => times(8760, () => 1);
 
+const dummyRate = {
+  name: 'some rate',
+  title: 'that will not converge',
+  rateElements: [
+    {
+      rateElementType: 'FixedPerMonth',
+      name: 'Customer Charge',
+      rateComponents: [
+        {
+          charge: 500,
+          name: 'Customer Charge',
+        },
+      ],
+    },
+    {
+      rateElementType: 'MonthlyDemand',
+      name: 'Demand Charge',
+      rateComponents: [
+        {
+          charge: 20,
+          name: 'Demand Charge',
+        },
+      ],
+    },
+    {
+      rateElementType: 'MonthlyEnergy',
+      name: 'Energy Charge',
+      rateComponents: [
+        {
+          charge: 0.18999,
+          name: 'Energy Charge',
+        },
+      ],
+    },
+  ],
+};
+
 describe('LoadProfileScaler', () => {
-  const initialLoadProfile = new LoadProfile(getLoadProfileOfOnes(), {year: 2019});
+  const initialLoadProfile = new LoadProfile(getLoadProfileOfOnes(), { year: 2019 });
   let loadProfileScaler;
 
   beforeEach(() => {
@@ -28,6 +66,17 @@ describe('LoadProfileScaler', () => {
       it('returns a LoadProfile', () => {
         const scaledLoadProfile = loadProfileScaler.toAverageMonthlyBill(100, e1);
         expect(scaledLoadProfile).toBeInstanceOf(LoadProfile);
+      });
+
+      it('converges with a large bill amount', () => {
+        const billAmount = 10000;
+        const scaledLoadProfile = loadProfileScaler.toAverageMonthlyBill(billAmount, dummyRate);
+
+        const rateCalculator = new RateCalculator({
+          ...dummyRate,
+          loadProfile: scaledLoadProfile,
+        } as RateCalculatorInterface);
+        expect(rateCalculator.annualCost() / 12).toBeCloseTo(billAmount);
       });
     });
   });
