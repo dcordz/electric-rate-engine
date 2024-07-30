433258401b316671a38fe83915562a6eef831640
Daniel Kuck-Alvarez <dankuck@gmail.com>
2024-07-30 19:32:10 -0400

    Remove comment

--- src/rateEngine/constants/index.ts
+++ src/rateEngine/constants/index.ts
@@ -5,10 +5,6 @@ export enum RateElementClassification {
   SURCHARGE = 'surcharge',
 };
 
-// Since the billing category is passed into a RateCalculator as part of the
-// arguments, this enum exists for documentation rather than actual type safety.
-// Once this compiles to JS, nothing will prevent an application from passing in
-// an abitrary string for the billingCategory.
 export enum BillingCategory {
   TAX = 'tax',
   SUPPLY = 'supply',
