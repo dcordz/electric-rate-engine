3c783a281508f418f3755ae027275379a94c2d04
Dave Bellawatt <17937472+dcordz@users.noreply.github.com>
2025-01-21 15:43:53 -0500

    rollup-rebased - export RateElement and RateComponent from src/index.ts, revert lib in tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export { default as RateCalculator } from './rateEngine/RateCalculator.ts';
 export { default as LoadProfile } from './rateEngine/LoadProfile.ts';
+export { default as RateElement } from './rateEngine/RateElement.ts';
+export { default as RateComponent } from './rateEngine/RateComponent.ts';
 
 export * from './rateEngine/constants/index.ts';
 export * from './rateEngine/types/index.ts';
 
-export const RateEngineVersion = "2.0.2"
