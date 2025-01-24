3d66dad07e94be6adc7eb22ffaa4a67419b6cd62
Dave Bellawatt <17937472+dcordz@users.noreply.github.com>
2025-01-24 17:01:12 -0500

    string-constants -  export rate element and rate component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export {default as RateCalculator} from './rateEngine/RateCalculator';
 export {default as LoadProfile} from './rateEngine/LoadProfile';
+export {default as RateElement} from './rateEngine/RateElement';
+export {default as RateComponent} from './rateEngine/RateComponent';
 
 export * from "./rateEngine/constants"
 export * from './rateEngine/types';
